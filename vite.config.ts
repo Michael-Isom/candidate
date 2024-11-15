018b03f025c047bfa81839810755badf29265fd0
Michael Isom <michaelisom@Michaels-MacBook-Pro.local>
2024-11-14 20:44:47 -0700

    Setup for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Set the base path for GitHub Pages deployment
+  base: '/candidate-search/',  // Replace with your repo name
+
+  // Directory for environment variables
   envDir: './env',
+
+  // React plugin
   plugins: [react()],
 });
